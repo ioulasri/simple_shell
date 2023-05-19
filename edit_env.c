5cb88650b51257dbbb7ca1f69fd071ceec218f44
nouhaila61 <nouhailaelbouhali99@gmail.com>
2023-05-19 11:24:15 +0100

    edit env

--- edit_env.c
+++ edit_env.c
@@ -45,4 +45,86 @@ int len_env(char **env)
 	return (i);
 }
 
+/**
+ * _setenv - sets an environment variable
+ * @name: name of the variable
+ * @value: value of the variable
+ * @overwrite: overwrite flag
+ * Return: 0 on success, -1 on failure
+ */
 
+int _setenv(char *key, char *value, int overwrite)
+{
+	int i = 0, j = 0, k = 0;
+	char *new_var = NULL;
+	char **env = NULL;
+
+	env = environ;
+	while (env[i] != NULL)
+	{
+		while (env[i][j] != '=')
+			j++;
+		if (_strncmp(env[i], (char *)key, j) == 0)
+		{
+			if (overwrite == 1)
+			{
+				new_var = malloc(sizeof(char) * (_strlen(key) + _strlen(value) + 2));
+				if (new_var == NULL)
+					return (-1);
+				for (k = 0; k < j; k++)
+					new_var[k] = env[i][k];
+				new_var[k] = '=';
+				k++;
+				for (j = 0; value[j] != '\0'; j++, k++)
+					new_var[k] = value[j];
+				new_var[k] = '\0';
+				env[i] = new_var;
+				return (0);
+			}
+			return (0);
+		}
+		i++;
+		j = 0;
+	}
+	new_var = malloc(sizeof(char) * (_strlen(key) + _strlen(value) + 2));
+	if (new_var == NULL)
+		return (-1);
+	for (k = 0; key[k] != '\0'; k++)
+		new_var[k] = key[k];
+	new_var[k] = '=';
+	k++;
+	for (j = 0; value[j] != '\0'; j++, k++)
+		new_var[k] = value[j];
+	new_var[k] = '\0';
+	env[i] = new_var;
+	env[i + 1] = NULL;
+	return (0);
+}
+
+/**
+ * _unsetenv - unsets an environment variable
+ * @name: name of the variable
+ * Return: 0 on success, -1 on failure
+ */
+
+int _unsetenv(char *key)
+{
+	int i = 0, j = 0, k = 0;
+	char **env = NULL;
+
+	env = environ;
+	while (env[i] != NULL)
+	{
+		while (env[i][j] != '=')
+			j++;
+		if (_strncmp(env[i], (char *)key, j) == 0)
+		{
+			for (k = i; env[k] != NULL; k++)
+				env[k] = env[k + 1];
+			return (0);
+		}
+		i++;
+		j = 0;
+	}
+	return (-1);
+}
