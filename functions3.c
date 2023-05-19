269f159cfe1a4a2f5220f12dc64a7dc7c332e23a
nouhaila61 <nouhailaelbouhali99@gmail.com>
2023-05-19 11:26:45 +0100

    functions

--- functions3.c
+++ functions3.c
@@ -25,36 +25,6 @@ char *_strpbrk(char *s, char *accept)
 	return (NULL);
 }
 
-/**
- * _strtok - tokenizes a string
- * @str: string to be tokenized
- * @delim: delimiter
- * Return: pointer to the tokenized string
- */
-
-char *_strtok(char *str, char *delim)
-{
-	static char *token;
-	static char *next_token;
-	char *delim_ptr = NULL;
-
-	if (str != NULL)
-		token = str;
-	else
-		token = next_token;
-	if (token == NULL)
-		return (NULL);
-	delim_ptr = _strpbrk(token, delim);
-	if (delim_ptr != NULL)
-	{
-		*delim_ptr = '\0';
-		next_token = delim_ptr + 1;
-	}
-	else
-		next_token = NULL;
-	return (token);
-}
-
 
 
 
