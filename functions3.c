8323805771420d155c0650eebf69a59243336c90
nouhaila61 <nouhailaelbouhali99@gmail.com>
2023-05-19 11:26:01 +0100

    functions

--- functions3.c
+++ functions3.c
@@ -83,36 +83,6 @@ char *_strcat(char *dest, char *src)
 	return (dest);
 }
 
-/**
- * _realloc - reallocates a memory block
- * @ptr: pointer to the memory previously allocated
- * @old_size: size of the allocated space for ptr
- * @new_size: new size of the new memory block
- * Return: pointer to the new memory block
- */
 
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
-{
-    char *new_ptr;
-    unsigned int i;
-
-    if (new_size == old_size)
-        return (ptr);
-
-    if (ptr == NULL)
-        return (malloc(new_size));
-
-    if (new_size == 0 && ptr != NULL)
-        return (NULL);
-
-    new_ptr = malloc(new_size);
-    if (new_ptr == NULL)
-        return (NULL);
-
-    for (i = 0; i < old_size && i < new_size; i++)
-        new_ptr[i] = ((char *)ptr)[i];
-
-    return (new_ptr);
-}
 
 
