901d4801f34fa44cf74e74ea688add27f687b20a
imad <imad.oulasri@iu-study.org>
2023-05-24 22:46:32 +0100

    please work

--- main.c
+++ main.c
@@ -33,6 +33,11 @@ int main(int argc, char **argv, char **env)
 		tokens = tokenize(line);
 		if (tokens == NULL)
 			continue;
+		if (tokens[0] == NULL)
+		{
+			free(tokens);
+			continue;
+		}
 		status = execute(tokens, argv, env);
 		free(tokens);
 	}
